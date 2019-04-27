ac96dc686634b792870414c29af36724b50cad16
Jurgen Welschen <jurgen.welschen@gmail.com>
2019-04-27 22:37:49 +0200

    almost there

--- Assets/Scripts/Planners/RobotPlanner.cs
+++ Assets/Scripts/Planners/RobotPlanner.cs
@@ -1,9 +1,13 @@
 using System;
-using System.Collections.Specialized;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Actuators;
+using DataModels;
+using RobotActions;
 using Sensors;
 using UnityEngine;
+using Timer = System.Timers.Timer;
 
 namespace Planners
 {
@@ -11,42 +15,86 @@ namespace Planners
     {
         private RobotActuator _robotVisionActuator;
         private RobotVisionSensor _robotVisionSensor;
-        
-       
+
+        private Queue<RobotAction> _robotActionQueue = new Queue<RobotAction>();
+
+        private Timer _actionExecuteTimer; 
         void Start()
         {
             _robotVisionActuator = transform.GetComponent<RobotActuator>();
             _robotVisionSensor = transform.Find(Settings.RobotFieldOfViewObjectName).GetComponent<RobotVisionSensor>();
             
-            this.InitializeObjectsOfInterestSubscriptions();
+            InitializeObjectsOfInterestSubscriptions();
         }
 
+        private void FixedUpdate()
+        {
+            
+        }
+
+        private void Update()
+        {
+            // Check if there are items in the queue at this moment
+            if (!_robotActionQueue.Any())
+                return;
+            
+            // Check if there is no timer for an action running already
+            if (_actionExecuteTimer != null && _actionExecuteTimer.Enabled)
+                return;
+            
+            // There is an action available and no timer is currently running, take it from the queue and send it to the actuator :)
+            var robotActionToExecute = _robotActionQueue.Dequeue();
+        }
+
+        /// <summary>
+        /// Binds event handlers to the properties of the object of interest.
+        /// </summary>
         private void InitializeObjectsOfInterestSubscriptions()
         {
             // Subscribe to the events of the objects of interest.
             foreach (var objectOfInterestVisionStatus in _robotVisionSensor.objectsOfInterestVisionStatus)
             {
-                objectOfInterestVisionStatus.IsInsideVisionAngleChangeEvent += ObjectInsideVisionChangeHandler;
-                objectOfInterestVisionStatus.IsWithinDistanceChangeEvent += ObjectWithinDistanceChangeEventHandler;
+                objectOfInterestVisionStatus.IsInsideVisionAngleChangeEvent += ObjectOfInterestPropertyChangeHandler;
+                objectOfInterestVisionStatus.IsWithinDistanceChangeEvent += ObjectOfInterestPropertyChangeHandler;
             }
         }
 
-        
-        /// <summary>
-        /// This function will be called whenever the property insidevision of an object we are interested is changed.
-        /// </summary>
-        /// <param name="sender">The object that was changed</param>
-        /// <param name="args">Any arguments</param>
-        private void ObjectInsideVisionChangeHandler(object sender, EventArgs args)
+        private void ObjectOfInterestPropertyChangeHandler(object sender, EventArgs args)
         {
-            print("Object inside vision called");
+            var objectOfInterestStatus = (ObjectOfInterestVisionStatus) sender;
+
+            // Check if we have new actions to add to the queue :)
+            var robotActionsToQueue = DetermineRobotActionForSoccerBallChange(objectOfInterestStatus);
+            
+            // Add all of the new actions to the queue.
+            if (robotActionsToQueue.Any())
+            {
+                robotActionsToQueue.ForEach(x => _robotActionQueue.Enqueue(x));
+                
+            }
         }
 
-        private void ObjectWithinDistanceChangeEventHandler(object sender, EventArgs args)
+        private List<RobotAction> DetermineRobotActionType(ObjectOfInterestVisionStatus objectOfInterestVisionStatus)
         {
-            print("Object within distance called");
+            // All action logic related to the soccerball
+            if (objectOfInterestVisionStatus.ObjectName != Settings.SoccerBallObjectName)
+                return DetermineRobotActionForSoccerBallChange(objectOfInterestVisionStatus);
+            
+            return null;
         }
-        
+
+        private List<RobotAction> DetermineRobotActionForSoccerBallChange(ObjectOfInterestVisionStatus objectOfInterestVisionStatus)
+        {
+            // If the soccerball is within distance
+            if (objectOfInterestVisionStatus.IsWithinDistance)
+                return new List<RobotAction>()
+                {
+                    new MoveForwardAction()
+                };
+
+            return null;
+        }
+
 //        private void TriggerMoveForward()
 //        {
 //            _robotVisionActuator.TriggerAction(RobotMotorAction.MoveForward);
---
+++ Assets/Scripts/RobotActions/MoveForwardAction.cs
@@ -0,0 +1,11 @@
+namespace RobotActions
+{
+    public class MoveForwardAction : RobotAction
+    {
+        public override float ActionDuration => 5.0f;
+        public override RobotArmAction ArmAction => RobotArmAction.None;
+        public override RobotLegAction LegAction => RobotLegAction.None;
+        public override RobotMotorAction MotorAction => RobotMotorAction.MoveForward;
+        public override RobotWheelAction WheelAction => RobotWheelAction.None;
+    }
+}
