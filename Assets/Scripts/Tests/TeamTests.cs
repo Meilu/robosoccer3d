3ae7f0d81774682fe004b48128a57680569caf05
Jurgen Welschen <jurgen.welschen@gmail.com>
2019-06-08 20:16:15 +0200

    prefab instantiaten in unit test kan blijkbaar ook

--- Assets/Scripts/Tests/TeamTests.cs
+++ Assets/Scripts/Tests/TeamTests.cs
@@ -4,78 +4,76 @@ using System.Linq;
 using DataModels;
 using NUnit.Framework;
 using Planners;
+using UnityEditor;
 using UnityEngine;
 using Team = PhysReps.Team;
 
 namespace Tests
 {
+    [TestFixture]
     public class TeamTests
     {
+        private GameObject _robot;
+        private Team _team;
+        
+        [SetUp]
+        public void Init()
+        {
+            _team = new Team();
+            
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/prefabs/robot.prefab");
+            _robot = GameObject.Instantiate(prefab, new Vector3(0, 0, 10), new Quaternion(0, 180, 0, 0));
+            
+        }
         [Test]
         public void AddKeeperPlannerComponentToRobot_ValidTeamPosition_ComponentAdded()
         {
-            var team = new Team();
-            var robotObject = new GameObject("robot");
-
-            team.AddPlannerComponentToRobot(TeamPosition.Keeper, robotObject);
+            _team.AddPlannerComponentToRobot(TeamPosition.Keeper, _robot);
             
-            Assert.IsTrue(robotObject.GetComponent<KeeperPlanner>() != null);
+            Assert.IsTrue(_robot.GetComponent<KeeperPlanner>() != null);
         }
         
         [Test]
         public void AddAttackerPlannerComponentToRobot_ValidTeamPosition_ComponentAdded()
         {
-            var team = new Team();
-            var robotObject = new GameObject("robot");
-
-            team.AddPlannerComponentToRobot(TeamPosition.Attacker, robotObject);
+            _team.AddPlannerComponentToRobot(TeamPosition.Attacker, _robot);
             
-            Assert.IsTrue(robotObject.GetComponent<AttackerPlanner>() != null);
+            Assert.IsTrue(_robot.GetComponent<AttackerPlanner>() != null);
         }
         
         [Test]
         public void AddMidfielderPlannerComponentToRobot_ValidTeamPosition_ComponentAdded()
         {
-            var team = new Team();
-            var robotObject = new GameObject("robot");
-
-            team.AddPlannerComponentToRobot(TeamPosition.Midfielder, robotObject);
+            _team.AddPlannerComponentToRobot(TeamPosition.Midfielder, _robot);
             
-            Assert.IsTrue(robotObject.GetComponent<MidfielderPlanner>() != null);
+            Assert.IsTrue(_robot.GetComponent<MidfielderPlanner>() != null);
         }
         
         [Test]
         public void AddDefenderPlannerComponentToRobot_ValidTeamPosition_ComponentAdded()
         {
-            var team = new Team();
-            var robotObject = new GameObject("robot");
-
-            team.AddPlannerComponentToRobot(TeamPosition.Defender, robotObject);
+            _team.AddPlannerComponentToRobot(TeamPosition.Defender, _robot);
             
-            Assert.IsTrue(robotObject.GetComponent<DefenderPlanner>() != null);
+            Assert.IsTrue(_robot.GetComponent<DefenderPlanner>() != null);
         }
         
         [Test]
         public void AddDefenderPlannerComponentToRobot_InValidTeamPosition_ComponentNotAdded()
         {
-            var team = new Team();
-            var robotObject = new GameObject("robot");
-
-            team.AddPlannerComponentToRobot(TeamPosition.Midfielder, robotObject);
+            _team.AddPlannerComponentToRobot(TeamPosition.Midfielder, _robot);
             
-            Assert.IsTrue(robotObject.GetComponent<DefenderPlanner>() == null);
+            Assert.IsTrue(_robot.GetComponent<DefenderPlanner>() == null);
         }
         
         [Test]
         public void GetFormationPositions_TwoPositions_ListPopulated()
         {
-            var team = new Team();
             var maxSquadNumber = 2;
             var colliderBounds = Vector3.zero;
             var robotPrefabSize = Vector3.zero;
             var robotYAxisPosition = 0.4f;
 
-            var formationPositions = team.GetFormationPositions(maxSquadNumber, colliderBounds, robotPrefabSize, robotYAxisPosition);
+            var formationPositions = _team.GetFormationPositions(maxSquadNumber, colliderBounds, robotPrefabSize, robotYAxisPosition);
             
             Assert.IsTrue(formationPositions.ToList().Count == maxSquadNumber);
         }
@@ -83,13 +81,12 @@ namespace Tests
         [Test]
         public void GetFormationPositions_ZeroPositions_ListNotPopulated()
         {
-            var team = new Team();
             var maxSquadNumber = 0;
             var colliderBounds = Vector3.zero;
             var robotPrefabSize = Vector3.zero;
             var robotYAxisPosition = 0.4f;
 
-            var formationPositions = team.GetFormationPositions(maxSquadNumber, colliderBounds, robotPrefabSize, robotYAxisPosition);
+            var formationPositions = _team.GetFormationPositions(maxSquadNumber, colliderBounds, robotPrefabSize, robotYAxisPosition);
             
             Assert.IsTrue(formationPositions.ToList().Count == maxSquadNumber);
         }
@@ -97,13 +94,12 @@ namespace Tests
         [Test]
         public void GetFormationPositionForSquadNumber_ValidParameters_FormationPositionCreated()
         {
-            var team = new Team();
             var squadNumber = 2;
             var colliderBounds = Vector3.zero;
             var robotPrefabSize = Vector3.zero;
             var robotYAxisPosition = 0.4f;
 
-            var formationPosition = team.CreateFormationPositionForSquadNumber(squadNumber, colliderBounds, robotPrefabSize, robotYAxisPosition);
+            var formationPosition = _team.CreateFormationPositionForSquadNumber(squadNumber, colliderBounds, robotPrefabSize, robotYAxisPosition);
 
             Assert.IsNotNull(formationPosition);
             Assert.IsTrue(formationPosition.Number == squadNumber);
@@ -112,7 +108,6 @@ namespace Tests
         [Test]
         public void FindFormationPositionVectorBySquadNumber_ValidSquadNumber_FormationPositionFound()
         {
-            var team = new Team();
             var squadNumber = 2;
             
             var formationPositions = new List<FormationPosition>()
@@ -124,7 +119,7 @@ namespace Tests
                 }
             };
             
-            var formationPosition = team.FindFormationPositionVectorBySquadNumber(formationPositions, squadNumber);
+            var formationPosition = _team.FindFormationPositionVectorBySquadNumber(formationPositions, squadNumber);
 
             Assert.IsNotNull(formationPosition);
         }
@@ -132,7 +127,6 @@ namespace Tests
         [Test]
         public void FindFormationPositionVectorBySquadNumber_InValidSquadNumber_FormationPositionNotFound()
         {
-            var team = new Team();
             var squadNumber = 2;
             
             var formationPositions = new List<FormationPosition>()
@@ -144,19 +138,18 @@ namespace Tests
                 }
             };
 
-            Assert.Throws<InvalidOperationException>(() => team.FindFormationPositionVectorBySquadNumber(formationPositions, 3));
+            Assert.Throws<InvalidOperationException>(() => _team.FindFormationPositionVectorBySquadNumber(formationPositions, 3));
         }
         
         [Test]
         public void CalculateFormationPositionForSquadNumber_ValidSquadNumber_PositionReturned()
         {
-            var team = new Team();
             var squadNumber = 2;
             var colliderBounds = Vector3.zero;
             var robotPrefabSize = Vector3.zero;
             var robotYAxisPosition = 0.4f;
 
-            var formationPosition = team.CalculateFormationPositionForSquadNumber(squadNumber, colliderBounds, robotPrefabSize, robotYAxisPosition);
+            var formationPosition = _team.CalculateFormationPositionForSquadNumber(squadNumber, colliderBounds, robotPrefabSize, robotYAxisPosition);
 
             Assert.IsNotNull(formationPosition);
         }
