b7573b159d9290a802414025a4a9dc86f1a86c5c
Wietske <wb.overtoom@gmail.com>
2019-06-10 20:41:37 +0200

    tests voor de bal toegevoegd. Alleen die ene check op direction is mss niet helemaal fantastisch

--- Assets/Scripts/Tests/BallTests.cs
+++ Assets/Scripts/Tests/BallTests.cs
@@ -1,7 +1,8 @@
 using NUnit.Framework;
 using PhysReps;
-using Planners;
 using UnityEngine;
+using NUnit.Framework;
+using Ball = PhysReps.Ball;
 
 namespace Tests
 {
@@ -9,7 +10,7 @@ namespace Tests
     public class BallTests
     {
         private Ball _ball;
-        
+
         // [SetUp] Zorgt ervoor dat die deze functie uitvoert voordat elk testje wordt uitgevoerd. dit is handig als we data willen klaarzetten die over testjes gedeeld wordt in dit bestand, zoals het ball object.
         [SetUp]
         public void Init()
@@ -17,16 +18,37 @@ namespace Tests
             // elke keer als een testje wordt uitgevoerd maakt die een nieuw ball object aan. deze heb je nodig om de logica te kunnen testen. hierin zitten de functies 
             _ball = new Ball();
         }
-        
+
         // Door [Test] boven een functie te zetten en dit bestandje in de tests folder te plaatsen weet unity dat dit een unit test is.
         [Test]
         public void IsHitByFrontLegs_ColliderIsFrontLegs_ReturnsTrue()
         {
-            // Hier kan je de isHitByFrontLegs functie testen van het ball object: _ball.isHitByFrontLegs(naamvanlegshier)
-            // met Assert.IsTrue(resultaathier) verwacht die dat de functie true gaat returnen, dus hoe kan je de IsHitByFrontLegs functie uitvoeren zodat die true returned?
-            // kijk ook in team.cs voor meer voorbeelden
+            var colliderName = "frontLegs";
+
+            _ball.IsHitByFrontLegs(colliderName);
+            Assert.IsTrue(_ball.IsHitByFrontLegs(colliderName));
         }
-        
-        // TODO: ook een functie maken om het false pad van de ishitbyfrontlegs functie te testen, zelfde testie als hierboven maar hoe ga je hem false laten returnen en false asserten? 
+
+        [Test]
+        public void IsHitByFrontLegs_ColliderIsNotFrontLegs_ReturnsTrue()
+        {
+            var colliderName = "frontArms";
+
+            _ball.IsHitByFrontLegs(colliderName);
+            Assert.IsTrue(!_ball.IsHitByFrontLegs(colliderName));
+        }
+
+        [Test]
+        public void GetBallShootingDirectionWithForce__ReturnsCorrectDirection()
+        {
+            float ShootingForce = _ball.ShootingForce;
+
+            Vector3 robotPosition = new Vector3(0.0f, 1.2f, 0.0f);
+            Vector3 ballPosition = new Vector3(0.0f, 1.0f, 0.0f);
+
+            var direction = _ball.GetBallShootingDirectionWithForce(robotPosition, ballPosition);
+            Assert.IsTrue(direction == -(robotPosition - ballPosition).normalized * ShootingForce);
+        }
+
     }
 }
