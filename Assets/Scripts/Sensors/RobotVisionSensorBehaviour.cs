840169f4c98d896d92e79a5c9582cde2e0395cd0
Jurgen <jurgen.welschen@gmail.com>
2019-06-16 04:40:09 +0200

    visionsensor opgesplitst

--- Assets/Scripts/Sensors/RobotVisionSensorBehaviour.cs
+++ Assets/Scripts/Sensors/RobotVisionSensorBehaviour.cs
@@ -10,149 +10,18 @@ namespace Sensors
 {
     public class  RobotVisionSensorBehaviour: RobotSensorBehaviour<ObjectOfInterestVisionStatus>
     {
-        [UnityEngine.Range(0,360)]
         public float viewAngle;
 
         private RobotVisionSensor RobotVisionSensor { get; set; }
-
+        private List<ObjectOfInterestVisionStatus> _objectOfInterestVisionStatusses;
         RobotVisionSensorBehaviour()
         {
-            RobotVisionSensor = new RobotVisionSensor();
+            RobotVisionSensor = new RobotVisionSensor(viewAngle);
         }
         
         private void Start()
         {
-            foreach (var objectOfInterestVisionStatus in objectsOfInterestStatus)
-            {   
-                objectOfInterestVisionStatus.GameObjectToFind = GameObject.Find(objectOfInterestVisionStatus.ObjectName);
-            }
-            
-            // Find all other robots by their tag name.
-            var otherRobotsByTag = GetObjectsOfInterestByTag(Settings.OtherRobotsTagName);
-            
-            // Merge them with our existing list so that their vision status will be updated.
-            objectsOfInterestStatus = objectsOfInterestStatus.Union(otherRobotsByTag).ToList();
-        }
-        
-        /// <summary>
-        /// Will continuously check all properties of the objects of interest we are interested in and update their status.
-        /// </summary>
-        protected override void UpdateObjectsOfInterestStatuses()
-        {
-            foreach (var objectOfInterestVisionStatus in objectsOfInterestStatus)
-            {
-                // If object not found, don't continue this iteration but go to the next.
-                if (!objectOfInterestVisionStatus.GameObjectToFind)
-                    continue;
-  
-                // Update the distance and vision angle status of this object of interest.
-              objectOfInterestVisionStatus.IsWithinDistance = IsObjectWithinDistance(objectOfInterestVisionStatus.GameObjectToFind, objectOfInterestVisionStatus.MinimunDistance);
-              objectOfInterestVisionStatus.IsInsideVisionAngle = IsObjectInsideVisionAngle(objectOfInterestVisionStatus.GameObjectToFind);   
-            }
-        }
-
-        /// <summary>
-        /// Checks whether the object is inside the robot's vision angle or not
-        /// </summary>
-        /// <param name="objectToFind"></param>
-        /// <returns>bool</returns>
-        private bool IsObjectInsideVisionAngle(GameObject objectToFind)
-        {
-            if (!objectToFind)
-                return false;
-
-            var currentTransform = transform;
-            
-            // Get current position of the robot.
-            var robotObjectPosition = currentTransform.parent.position;
-            
-            // Get the direction to the object
-            var directionToObject = (objectToFind.transform.position - robotObjectPosition);
-            
-            // Calculate the angle to the object and check if it's inside our viewAngle.
-            var isInAngle = Vector3.Angle(currentTransform.forward, directionToObject) < viewAngle;
-
-            // If the object is already inside of our viewangle, just return immediately.
-            if (isInAngle)
-                return true;
-
-            if (!IsObjectWithinDistance(objectToFind, 1.0f))
-                return false;
-
-            if (!IsRobotFacingObjectToFind(objectToFind, robotObjectPosition, currentTransform))
-                return false;
-            
-            // Some object can be quite large and the geometric center will not be inside our angle, even though a part of the object could still be in our vision.
-            // We can double check with raycasts if this is the case, but only if we are facing this object and the object is close to us.
-            return IsObjectInsideRaycastAngle(objectToFind, robotObjectPosition, currentTransform);
-        }
-
-        private bool IsObjectInsideRaycastAngle(GameObject objectToFind, Vector3 robotObjectPosition, Transform currentTransform)
-        {
-            Vector3 rayPosition = new Vector3(robotObjectPosition.x, currentTransform.position.y, robotObjectPosition.z);
-            Vector3 leftRayRotation = Quaternion.AngleAxis(-viewAngle, currentTransform.up) * currentTransform.forward;
-            Vector3 rightRayRotation = Quaternion.AngleAxis(viewAngle, currentTransform.up) * currentTransform.forward;
-            
-            var rays = new List<Ray>()
-            {
-                new Ray(rayPosition, transform.forward),
-                new Ray(rayPosition, leftRayRotation),
-                new Ray(rayPosition, rightRayRotation)
-            };
-
-            foreach (var ray in rays)
-            {
-                if (!Physics.Raycast(ray, out var hit))
-                    continue;
-                
-                // If this collider hit the object we were looking for
-                if (hit.collider.name != objectToFind.name)
-                    continue;
-                
-                return true;
-            }
-
-            return false;
-        }
-
-        private bool IsRobotFacingObjectToFind(GameObject objectToFind, Vector3 robotObjectPosition, Transform currentTransform)
-        {
-            Vector3 rayPosition = new Vector3(robotObjectPosition.x, currentTransform.position.y, robotObjectPosition.z);
-            var forwardRay = new Ray(rayPosition, transform.forward);
-            
-            if (Physics.Raycast(forwardRay, out var hit))
-                return hit.collider.name == objectToFind.name;
-            
-            return false;
-        }
-
-        /// <summary>
-        /// Checks whether the object is inside the given max distance.
-        /// </summary>
-        /// <param name="objectToFind">The object </param>
-        /// <param name="maxDistanceToCheck"></param>
-        /// <returns></returns>
-        private bool IsObjectWithinDistance(GameObject objectToFind, float maxDistanceToCheck)
-        {
-            if (!objectToFind)
-                return false;
-            
-            // Get current position of the robot.
-            var robotObjectPosition = transform.parent.position;
-            
-            // Check whether the object is within the given max distance.
-            return Vector3.Distance(objectToFind.transform.position, robotObjectPosition) < maxDistanceToCheck;
-        }
-    }
-
-    public class RobotVisionSensor : RobotSensor
-    {
-
-        private List<ObjectOfInterestVisionStatus> _objectOfInterestVisionStatuses;
-
-        public RobotVisionSensor()
-        {
-            _objectOfInterestVisionStatuses = new List<ObjectOfInterestVisionStatus>() {
+            var tempObjectOfInterestStatusses = new List<ObjectOfInterestVisionStatus>() {
                 new ObjectOfInterestVisionStatus()
                 {
                     ObjectName = Settings.SoccerBallObjectName,
@@ -168,6 +37,97 @@ namespace Sensors
                     ObjectName = Settings.HomeGoalLine
                 }
             };
+
+            foreach (var objectOfInterestVisionStatus in tempObjectOfInterestStatusses)
+            {   
+                objectOfInterestVisionStatus.GameObjectToFind = GameObject.Find(objectOfInterestVisionStatus.ObjectName);
+            }
+            
+            // Find all other robots by their tag name.
+            var otherRobotsByTag = RobotVisionSensor.CreateObjectOfInterestForGameObjects(GameObject.FindGameObjectsWithTag(Settings.OtherRobotsTagName));
+
+            // Merge lists together.
+            _objectOfInterestVisionStatusses = tempObjectOfInterestStatusses.Union(otherRobotsByTag).ToList();
+        }
+        
+        /// <summary>
+        /// Will continuously check all properties of the objects of interest we are interested in and update their status.
+        /// </summary>
+        protected override void UpdateObjectsOfInterestStatuses()
+        {
+            foreach (var objectOfInterestVisionStatus in _objectOfInterestVisionStatusses)
+            {
+                // If object not found, don't continue this iteration but go to the next.
+                if (!objectOfInterestVisionStatus.GameObjectToFind)
+                    continue;
+  
+                // Update the distance and vision angle status of this object of interest.
+                objectOfInterestVisionStatus.IsWithinDistance = RobotVisionSensor.IsObjectWithinDistance(objectOfInterestVisionStatus.GameObjectToFind.transform.position, transform.position, objectOfInterestVisionStatus.MinimunDistance);
+                objectOfInterestVisionStatus.IsInsideVisionAngle = IsObjectInsideVisionAngle(objectOfInterestVisionStatus.GameObjectToFind.transform.position, objectOfInterestVisionStatus.ObjectName, transform.position, transform.up, transform.forward);
+            }
+        }
+
+        private bool IsObjectInsideVisionAngle(Vector3 objectToFindPosition, string objectToFindName, Vector3 fromPosition, Vector3 upPointingVector, Vector3 forwardPointingVector)
+        {
+            if (RobotVisionSensor.IsObjectInsideVisionAngle(objectToFindPosition, fromPosition, forwardPointingVector))
+                return true;
+
+            if (!RobotVisionSensor.IsObjectWithinDistance(objectToFindPosition, fromPosition, 1.0f))
+                return false;
+
+            if (!IsRobotFacingObjectToFind(objectToFindName, fromPosition, forwardPointingVector))
+                return false;
+
+            // Some object can be quite large and the geometric center will not be inside our angle, even though a part of the object could still be in our vision.
+            // We can double check with raycasts if this is the case, but only if we are facing this object and the object is close to us.
+            return IsObjectInsideRaycastAngle(objectToFindName, fromPosition, upPointingVector, forwardPointingVector);
+        }
+        private bool IsRobotFacingObjectToFind(string objectToFindName, Vector3 fromPosition, Vector3 forwardPointingVector)
+        {
+            var forwardRay = new Ray(fromPosition, forwardPointingVector);
+
+            if (Physics.Raycast(forwardRay, out var hit))
+                return hit.collider.name == objectToFindName;
+
+            return false;
+        }
+
+        private bool IsObjectInsideRaycastAngle(string objectToFindName, Vector3 fromPosition, Vector3 upPointingVector, Vector3 forwardPointingVector)
+        {
+            Vector3 leftRayRotation = Quaternion.AngleAxis(-viewAngle, upPointingVector) * forwardPointingVector;
+            Vector3 rightRayRotation = Quaternion.AngleAxis(viewAngle, upPointingVector) * forwardPointingVector;
+            
+            var rays = new List<Ray>()
+            {
+                new Ray(fromPosition, transform.forward),
+                new Ray(fromPosition, leftRayRotation),
+                new Ray(fromPosition, rightRayRotation)
+            };
+
+            foreach (var ray in rays)
+            {
+                if (!Physics.Raycast(ray, out var hit))
+                    continue;
+                
+                // If this collider hit the object we were looking for
+                if (hit.collider.name != objectToFindName)
+                    continue;
+                
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public class RobotVisionSensor : RobotSensor
+    {
+        public List<ObjectOfInterestVisionStatus> ObjectOfInterestVisionStatuses;
+        private float _viewAngle;
+
+        public RobotVisionSensor(float viewAngle)
+        {
+            _viewAngle = viewAngle;
         }
         public IEnumerable<ObjectOfInterestVisionStatus> CreateObjectOfInterestForGameObjects(GameObject[] gameObjects)
         {
@@ -179,7 +139,27 @@ namespace Sensors
                 };
             }
         }
+        /// <summary>
+        /// Checks whether the object is inside the given max distance.
+        /// </summary>
+        public bool IsObjectWithinDistance(Vector3 objectToFindPosition, Vector3 robotObjectPosition, float maxDistanceToCheck)
+        {
+            // Check whether the object is within the given max distance.
+            return Vector3.Distance(objectToFindPosition, robotObjectPosition) < maxDistanceToCheck;
+        }
 
+        /// <summary>
+        /// Checks whether the object is inside the robot's vision angle or not
+        /// </summary>
+        /// <param name="objectToFind"></param>
+        /// <returns>bool</returns>
+        public bool IsObjectInsideVisionAngle(Vector3 objectToFindPosition, Vector3 fromPosition, Vector3 forwardPointingVector)
+        {
+            // Get the direction to the object
+            var directionToObject = (objectToFindPosition - fromPosition);
+
+            // Calculate the angle to the object and check if it's inside our viewAngle.
+            return Vector3.Angle(forwardPointingVector, directionToObject) < _viewAngle;
+        }
     }
-
 }
