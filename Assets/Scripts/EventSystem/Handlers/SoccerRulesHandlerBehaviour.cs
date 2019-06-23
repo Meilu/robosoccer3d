d9736f069b9791ea1b220bea89b79ac456c11f82
Wietske <wb.overtoom@gmail.com>
2019-06-23 23:35:39 +0200

    made beginning of soccer rules handler behaviour script

---
+++ Assets/Scripts/EventSystem/Handlers/SoccerRulesHandlerBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EventSystem.Handlers
+{
+    public class SoccerRulesHandlerBehaviour : MonoBehaviour
+    {
+        private SoccerRulesHandler _rulesHandler;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_rulesHandler.checkForRulesToStartGame()) {
+                _rulesHandler.checkForRulesDuringTheGame();
+            };
+        }
+    }
+
+    public class SoccerRulesHandler
+    {
+        public bool checkForRulesToStartGame()
+        {
+            return true;
+        }
+
+        public void checkForRulesDuringTheGame()
+        {
+            Console.Write("checking rules during the game");
+        }
+    }
+}
