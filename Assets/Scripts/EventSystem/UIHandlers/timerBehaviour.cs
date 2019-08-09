bc0dbf08ad6cbc4b3c326bf47a53a3216dddd88a
Wizzieh <wb.overtoom@gmail.com>
2019-08-09 22:45:03 +0200

    UI aangepast en TimerBehaviour zou moeten werken

---
+++ Assets/Scripts/EventSystem/UIHandlers/timerBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using EventSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class timerBehaviour : MonoBehaviour
+{
+    private Text _textComponent;
+    private Timer _timer;
+
+    private bool GameHasEnded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _textComponent = GetComponent<Text>();
+        _textComponent.text = "START";
+        //InvokeRepeating("_timer.startCountdown(_textComponent)", 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameHasEnded)
+        {
+            InvokeRepeating("UpdateTimerText()", 1.0f, 1.0f);
+        }
+        //not sure if this is needed anymore...
+        else CancelInvoke("UpdateTimerText()");
+    }
+
+    void UpdateTimerText()
+    {
+        //sets the text for the timer
+        _textComponent.text = _timer.DetermineMatchTimerText();
+    }
+}
+
+public class Timer {
+
+    private float duration = 30f;
+
+    public string DetermineMatchTimerText()
+    {
+        string MatchPartName = DetermineMatchPartName();
+        string CountDownTimer = UpdateCountdownTime();
+
+        return MatchPartName + "/n" + CountDownTimer;
+    }
+
+    public string DetermineMatchPartName()
+    {
+        string name = "MATCH IN PLAY";
+        return name;
+    }
+
+    public string UpdateCountdownTime()
+    {
+        float minutes = Mathf.Floor(duration / 60);
+        float seconds = Mathf.RoundToInt(duration%60);
+
+        string minutesDisplay = "0";
+        string secondsDisplay = "0";
+        if(minutes < 100) {
+            minutesDisplay = ""+ minutes;
+        }
+        if(seconds < 100) {
+            secondsDisplay = "" + Mathf.RoundToInt(seconds).ToString();
+        }
+
+        return minutesDisplay + ":" + secondsDisplay;
+    }
+}
+
--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -1,57 +1,79 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//
-//public class timer : MonoBehaviour
-//{
-//    // private Text _textComponent;
-//
-//    public float matchDuration;
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//     //   _textComponent = GetComponent<Text>();
-//        InvokeRepeating("decreaseMatchDuration", 1.0f, 1.0f);
-//    }
-//
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if (matchDuration == 0)
-//        {
-//       //     _textComponent.text = "finished";
-//        }
-//    }
-//
-//    void decreaseMatchDuration()
-//    {
-//        if (matchDuration > 0)
-//        {
-//            matchDuration--;
-//            updateTimerLabel();
-//        }
-//        else
-//        {
-//            CancelInvoke("decreaseMatchDuration");
-//            EventManager.Instance.TriggerEvent(EventType.MatchFinished);
-//            print("match finished");
-//        }
-//    }
-//
-//    void updateTimerLabel()
-//    {
-//        float minutes = Mathf.Floor(matchDuration / 60);
-//        float seconds = Mathf.RoundToInt(matchDuration%60);
-//
-//        string minutesDisplay = "0";
-//        string secondsDisplay = "0";
-//        if(minutes < 100) {
-//            minutesDisplay = ""+ minutes;
-//        }
-//        if(seconds < 100) {
-//            secondsDisplay = "" + Mathf.RoundToInt(seconds).ToString();
-//        }
-//
-//       //  _textComponent.text = minutesDisplay + ":" + secondsDisplay;
-//    }
-//}
+﻿using EventSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace Eventsystem.UIHandlers
+{
+    public class TimerBehaviour : MonoBehaviour
+    {
+        private Text _textComponent;
+        private Timer _timer;
+
+        public float matchDuration;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _textComponent = GetComponent<Text>();
+            _timer.countdownFromMatchDuration(_textComponent);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _timer.showMatchTimer(_textComponent.text);
+        }
+    }
+
+    public class Timer
+    {
+
+        void countdownFromMatchDuration(Text text)
+        {
+            InvokeRepeating("decreaseMatchDuration(_textComponent)", 1.0f, 1.0f);
+        }
+
+        void showMatchTimer(Text text)
+        {
+            if (matchDuration == 0)
+            {
+                return "finished";
+            }
+        }
+    
+        void decreaseMatchDuration(Text text)
+        {
+            if (matchDuration > 0)
+            {
+                matchDuration--;
+                updateTimerLabel();
+            }
+            else
+            {
+                CancelInvoke("decreaseMatchDuration");
+                //EventManager.Instance.TriggerEvent(EventType.MatchFinished);
+                print("match finished");
+            }
+        }
+
+        void updateTimerLabel()
+        {
+            float minutes = Mathf.Floor(matchDuration / 60);
+            float seconds = Mathf.RoundToInt(matchDuration%60);
+
+            string minutesDisplay = "0";
+            string secondsDisplay = "0";
+            if(minutes < 100) {
+                minutesDisplay = ""+ minutes;
+            }
+            if(seconds < 100) {
+                secondsDisplay = "" + Mathf.RoundToInt(seconds).ToString();
+            }
+
+            _textComponent.text = minutesDisplay + ":" + secondsDisplay;
+        }
+    }
+}
+
