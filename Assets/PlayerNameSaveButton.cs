195dddb82aae8d0d2d5db6053b697b9730e37793
Wizzieh <wb.overtoom@gmail.com>
2019-08-11 14:14:55 +0200

    knop is weer zichtbaar en in te drukken

--- Assets/PlayerNameSaveButton.cs
+++ Assets/PlayerNameSaveButton.cs
@@ -9,7 +9,7 @@ public class PlayerNameSaveButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
     }
 
     // Update is called once per frame
